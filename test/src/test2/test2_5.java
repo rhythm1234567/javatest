c4a9ea885e2d554bd299c1bd2c3e772a1d09f467
Rhythm <Rhythm@LAPTOP-IF52ORDG>
2019-04-18 00:31:15 +0800

    java

--- test/src/test2/test2_5.java
+++ test/src/test2/test2_5.java
@@ -1,24 +1,20 @@
 package test2;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.text.*;
-import java.util.*;
-
+import java.time.*;
+import java.util.Scanner;
 
 
 public class test2_5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("输入一个yyyy-MM-dd（年月日）格式的日期字符串:");
-BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-Date date =new Date();
-SimpleDateFormat t1=new  SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-SimpleDateFormat t2=new  SimpleDateFormat("Gyy-MM-FF HH:mm:ss");
-System.out.println(t1.format(date));
-System.out.println(t2.format(date));
-
+		Scanner s=new Scanner(System.in);
+		LocalDate time=LocalDate.parse(s.nextLine());
+		time=time.plusDays(7);
+		System.out.println(time);
+		LocalDate today=LocalDate.now();
+		System.out.println(today);
 	}
+
 
 }
