bf3c4b6f63ec2a87ac46f07644298a14cc71adb3
Rhythm <Rhythm@LAPTOP-IF52ORDG>
2019-04-18 23:47:47 +0800

    3

---
+++ test2/src/test2/test2_3.java
@@ -0,0 +1,22 @@
+package test2;
+
+public class test2_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		Integer N=Integer.valueOf(n);
+		int n2=N.intValue();
+		System.out.println(N.getClass().getName());
+		
+		String s2="123";
+		Integer S=Integer.valueOf(s2);
+		String s3=String.valueOf(S);
+		
+		int x=1;
+		String s=Integer.toString(x);
+		int x2=Integer.parseInt(s);
+		
+	}
+
+}
