c8d468d8b3b53281475a864ca75109abbeb28ad3
Rhythm <Rhythm@LAPTOP-IF52ORDG>
2019-04-23 23:49:27 +0800

    4

---
+++ test2/src/test2/test4_23.java
@@ -0,0 +1,49 @@
+package test2;
+
+
+	
+	
+	class MyException extends Exception{
+
+		public MyException(String string) {
+			// TODO Auto-generated constructor stub
+			super(string);
+		}
+		
+	}
+	 class tool{
+		public static int parseInt(String str) throws MyException
+		{int x=0;
+		for(int i=0;i<str.length();i++) {
+			if(Character.isDigit(str.charAt(i))) {
+			
+				return x;
+			}else {
+				throw new MyException("输入有误");
+			}
+			
+			}
+		return 0;
+		}
+	}
+	public class test4_23 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		tool t=new tool();
+		try {
+			int x=t.parseInt("z2");
+		}catch(MyException e) {
+		System.out.println(e);
+		}
+	}
+	}
+
+
+
+
+
+
+
+
+
+
