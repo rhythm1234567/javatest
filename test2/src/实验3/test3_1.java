d48b4ae4f116653f9361edb79961598edee86282
Rhythm <Rhythm@LAPTOP-IF52ORDG>
2019-04-29 00:37:44 +0800

    1

--- test2/src/实验3/test3_1.java
+++ test2/src/实验3/test3_1.java
@@ -1,7 +1,12 @@
 package 实验3;
 
-public class test3_1 {
+import java.awt.*;
 
+import javax.swing.*;
+
+public class test3_1 {
+	JFrame f=new JFrame("test");
+	JPanel p1=new JPanel(new FlowLayout());
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- test2/src/实验3/test3_3.java
+++ test2/src/实验3/test3_3.java
@@ -1,10 +1,28 @@
 package 实验3;
 
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class test3_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s;
+		Scanner in=new Scanner(System.in);
+		s=in.nextLine();
+		StringTokenizer  st= new StringTokenizer(s);
+		System.out.println(st.countTokens());
+		while(st.hasMoreElements()) {
+			System.out.println(st.nextElement());
+		}
+		
+		String s2=" wa     zhi wan  t  smal bro b     ";
+		
+		String [] sarry=s2.trim().split("\\s+");
+		System.out.println(sarry.length);
+		for(int i=0;i<sarry.length;i++) {
+			System.out.println(sarry[i]);
+		}
 	}
 
 }
