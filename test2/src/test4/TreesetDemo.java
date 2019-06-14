054318e26abd71ad858d372a5ea96f0fc765e5b0
Rhythm <Rhythm@LAPTOP-IF52ORDG>
2019-06-14 21:26:54 +0800

    1

--- test2/src/test4/TreesetDemo.java
+++ test2/src/test4/TreesetDemo.java
@@ -1,5 +1,9 @@
 package test4;
 
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 class Student implements Comparable{
 	int id;
 	String name;
@@ -16,20 +20,37 @@ class Student implements Comparable{
 		int result=0;
 		if(s.score==this.score) {
 			result=s.name.compareTo(this.name);
-			
+	
 		}
 		else {
 			result=s.score>this.score?1:0;
 		}
-		return 0;
+		return result;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", score=" + score + "]";
 	}
 	
 }
 public class TreesetDemo {
 
 	public static void main(String[] args) {
+		Student s1=new Student(1,"zz",12);
+		Student s2=new Student(2,"xx",10);
+		Student s3=new Student(3,"ww",12);
+		Set<Student> set=new TreeSet<Student>();
+		set.add(s1);
+		set.add(s2);
+		set.add(s3);
+		Iterator<Student>it=set.iterator();
+		while(it.hasNext()) {
+			Student s=it.next();
+			System.out.println(s);
+		}
 		
-
 	}
 
 }
