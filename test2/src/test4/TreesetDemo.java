80fb0164f7c6369423c0b2ced1fe1497f5f9f1b5
Rhythm <Rhythm@LAPTOP-IF52ORDG>
2019-06-14 21:04:46 +0800

    1

--- test2/src/test4/TreesetDemo.java
+++ test2/src/test4/TreesetDemo.java
@@ -1,5 +1,30 @@
 package test4;
 
+class Student implements Comparable{
+	int id;
+	String name;
+	float score;
+	public Student(int id, String name, float score) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.score = score;
+	}
+	@Override
+	public int compareTo(Object o) {
+		Student s=(Student)o;
+		int result=0;
+		if(s.score==this.score) {
+			result=s.name.compareTo(this.name);
+			
+		}
+		else {
+			result=s.score>this.score?1:0;
+		}
+		return 0;
+	}
+	
+}
 public class TreesetDemo {
 
 	public static void main(String[] args) {
