f7e16201601c0426c3a5260edf08c7673dc430f5
Rhythm <Rhythm@LAPTOP-IF52ORDG>
2019-06-18 21:17:54 +0800

    5

--- test2/src/test4/ArrayListDemo.java
+++ test2/src/test4/ArrayListDemo.java
@@ -24,7 +24,6 @@ class student implements Comparable<Object>{
 			return sno>stu.sno?1:-1;
 	
 	}
-
 	@Override
 	public String toString() {
 		return "student [sno=" + sno + ", name=" + name + ", score=" + score + "]";
